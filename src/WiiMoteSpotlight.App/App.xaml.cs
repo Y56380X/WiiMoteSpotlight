a3aee693dd512060d6c290beebc557ad03fa6bf1
Y56380X <Y56380X@users.noreply.github.com>
2020-03-01 16:54:41 +0100

    dependency injection for IWiiMote

--- src/WiiMoteSpotlight.App/App.xaml.cs
+++ src/WiiMoteSpotlight.App/App.xaml.cs
@@ -1,14 +1,40 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using WiiMoteSpotlight.Lib;
 
 namespace WiiMoteSpotlight.App
 {
 	public class App : Application
 	{
+		private static bool UseVirtual { get; } = true;
+		public ServiceProvider Services { get; private set; }
+
 		public override void Initialize()
 		{
 			AvaloniaXamlLoader.Load(this);
+			var serviceCollection = new ServiceCollection();
+			ConfigureServices(serviceCollection);
+			Services = serviceCollection.BuildServiceProvider();
+		}
+
+		private static void ConfigureServices(IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddTransient(CreateWiiMote);
+		}
+
+		private static IWiiMote CreateWiiMote(IServiceProvider serviceProvider)
+		{
+			if (UseVirtual)
+				return new Lib.Virtual.WiiMote();
+
+			return Environment.OSVersion.Platform switch
+			{
+				PlatformID.Unix => new Lib.XWiiMote.WiiMote(),
+				_ => throw new PlatformNotSupportedException()
+			};
 		}
 
 		public override void OnFrameworkInitializationCompleted()
