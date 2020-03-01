8d2bd84a3e897ffe0dec4f35549e75b929c75972
Y56380X <Y56380X@users.noreply.github.com>
2020-03-01 19:02:42 +0100

    refactoring

--- src/WiiMoteSpotlight.Lib.XWiiMote/WiiMote.cs
+++ src/WiiMoteSpotlight.Lib.XWiiMote/WiiMote.cs
@@ -12,24 +12,22 @@ namespace WiiMoteSpotlight.Lib.XWiiMote
 
 		public WiiMote()
 		{
-			var s = new monitor(true, true);
-			var sysPath = s.poll();
-			s.Dispose();
+			// Get wiimote system path
+			using var monitor = new monitor(true, true);
+			var sysPath = monitor.poll();
+			monitor.Dispose();
 			
+			// Open wiimote
 			_device = new iface(sysPath);
-
-			Console.WriteLine($"Use Device {_device.get_syspath()}");
-			Console.WriteLine($"fd: {_device.get_fd()}");
-			Console.WriteLine("Open Writeable");
-
 			_device.open((uint)(xwii_iface_type.IFACE_CORE | xwii_iface_type.IFACE_WRITABLE | xwii_iface_type.IFACE_MOTION_PLUS));
-
+			
 			_device.set_mp_normalization(-266, 2500, -1160, 50);
 		}
 		
 		public void Dispose()
 		{
 			_device.Dispose();
+			Console.WriteLine("WiiMote disposed");
 		}
 	}
 }
