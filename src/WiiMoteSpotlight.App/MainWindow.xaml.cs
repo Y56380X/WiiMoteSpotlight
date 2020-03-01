7371b326123da2801f882b44734e2679e9033316
Y56380X <Y56380X@users.noreply.github.com>
2020-03-01 17:08:57 +0100

    subscribe to OnKeyPress

--- src/WiiMoteSpotlight.App/MainWindow.xaml.cs
+++ src/WiiMoteSpotlight.App/MainWindow.xaml.cs
@@ -1,18 +1,24 @@
-using Avalonia;
-using Avalonia.Controls;
+using System;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
 using WiiMoteSpotlight.Lib;
 
 namespace WiiMoteSpotlight.App
 {
-	public class MainWindow : Window
+	public class MainWindow : FullscreenWindow
 	{
 		private IWiiMote WiiMote { get; } = App.Services.GetService<IWiiMote>();
 
 		public MainWindow()
 		{
 			InitializeComponent();
+			
+			// Subscribe to WiiMote events
+			WiiMote.KeyPress += WiiMoteOnKeyPress;
+		}
+
+		private void WiiMoteOnKeyPress(object? sender, ConsoleKey e)
+		{
 		}
 
 		private void InitializeComponent()
